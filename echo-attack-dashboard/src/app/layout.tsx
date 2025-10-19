b2752bb57583ef9fa28f8f8335f3afce162fae0b
vanessa <vanessarodrigues010506@gmail.com>
2025-10-19 20:59:09 +0530

    v1.0.7 scrolled area fixed

--- echo-attack-dashboard/src/app/layout.tsx
+++ echo-attack-dashboard/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="min-h-screen">
+        <div className="overflow-x-hidden min-h-screen">
           {children}
         </div>
       </body>
